e37bfd38a3d0670e0697f95660652d8670a5cc7a
Julien Hubert <3481141+Atlanta@users.noreply.github.com>
2020-10-04 18:14:44 +0200

    Added box machine

---
+++ Assets/Scripts/BoxController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxController : MonoBehaviour
+{
+    public void OnCollisionEnter(Collision collision)
+    {
+        Draggable draggable = GetComponent<Draggable>();
+        
+        Debug.Log("Enabling draggable ?");
+
+        if (collision.collider.tag == "Ground")
+        {
+            Debug.Log("Enabling draggable");
+            draggable.enabled = true;
+        }
+    }
+}
