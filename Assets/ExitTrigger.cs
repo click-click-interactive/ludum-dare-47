9ccde6b6eb36237177938f73be194cb2ed0ec490
Julien Hubert <3481141+Atlanta@users.noreply.github.com>
2020-10-05 17:53:40 +0200

    Added Rainbow

---
+++ Assets/ExitTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitTrigger : MonoBehaviour
+{
+    public TriggerAnimationOnCollisionsThreshold CubeExit;
+
+    void OnMouseDown()
+    {
+        CubeExit.isTriggered = true;
+    }
+}
---
+++ Assets/Shaders/OutlineRenderer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutlineRenderer : MonoBehaviour
+{
+    [SerializeField] private Material outlineMaterial;
+    [SerializeField] private float outlineScaleFactor;
+    [SerializeField] private Color outlineColor;
+    private Renderer outlineRenderer;
+
+    void Start()
+    {
+        outlineRenderer = CreateOutline(outlineMaterial, outlineScaleFactor, outlineColor);
+    }
+
+    Renderer CreateOutline(Material outlineMat, float scaleFactor, Color color)
+    {
+        GameObject outlineObject = Instantiate(this.gameObject, transform.position, transform.rotation ,transform);
+        Renderer rend = outlineObject.GetComponent<Renderer>();
+        rend.material = outlineMat;
+        rend.material.SetColor("_OutlineColor", color);
+        rend.material.SetFloat("_ScaleFactor", scaleFactor);
+        rend.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+        outlineObject.GetComponent<OutlineRenderer>().enabled = false;
+        outlineObject.GetComponent<Collider>().enabled = false;
+        rend.enabled = false;
+
+        return rend;
+    }
+
+    private void OnMouseEnter()
+    {
+        outlineRenderer.enabled = true;
+    }
+
+    private void OnMouseExit()
+    {
+        outlineRenderer.enabled = false;
+    }
+}
